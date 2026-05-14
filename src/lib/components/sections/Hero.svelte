09d0d6e7dc2b13a76558b275158f179b4a29403c
Robert Dominic <devrobert.codes@gmail.com>
2026-05-14 12:11:14 +0000

    Increase hero carousel card sizes

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -139,7 +139,7 @@
 						alt={project.alt}
 						width="400"
 						height="280"
-						class="h-40 w-96 rounded-2xl object-cover transition duration-300 group-hover:scale-[1.03] cursor-pointer"
+						class="h-44 w-[26rem] rounded-2xl object-cover transition duration-300 group-hover:scale-[1.03] cursor-pointer sm:h-48 sm:w-[28rem] lg:h-56 lg:w-[33rem]"
 					/>
 				</button>
 			{/each}
